ef0ee00c438f0559acc199d2b73cd265bf9f1757
Arianna Gao <gaoan01@gmail.com>
2020-08-11 20:55:08 +0800

    done with wechat-login

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -5,14 +5,25 @@ Page({
    * Page initial data
    */
   data: {
-
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
-
+  onLoad: function() {
+    // 查看是否授权
+    wx.getSetting({
+      success (res){
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function(res) {
+              console.log(res.userInfo)
+            }
+          })
+        }
+      }
+    })
+  },
+  bindGetUserInfo (e) {
+    console.log(e.detail.userInfo)
   },
 
   /**

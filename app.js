3fdcf0b0a06db399b4e5cc9d125ba821221f36b2
Vincent Trijard <58102816+vincenttrijard@users.noreply.github.com>
2020-08-14 16:57:32 +0800

    quickchanges

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ App({
       success: (res) => {
         console.log('login', res)
       wx.request({
-        // url: "https://hesha.wogengapp.cn/api/v1/login",
-        url: "http://localhost:3000/api/v1/login",
+        url: "https://hesha.wogengapp.cn/api/v1/login",
+        // url: "http://localhost:3000/api/v1/login",
         method: 'post',
         data: {
           code: res.code
@@ -24,8 +24,8 @@ App({
   globalData: {
     userInfo: null,
     userId: null,
-    base_url: "http://localhost:3000/api/v1"
-    // base_url: "https://hesha.wogengapp.cn/api/v1" 
+    // base_url: "http://localhost:3000/api/v1"
+    base_url: "https://hesha.wogengapp.cn/api/v1" 
     }
 })
 
